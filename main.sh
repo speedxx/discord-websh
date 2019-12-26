55d173f3a2146b3dbe9acc7b94863a73933e633d
speedxx <d.kilmetov@gmail.com>
2019-12-26 18:11:32 -0500

    better

--- main.sh
+++ main.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 WELCOME="!!welcome to discord-websh, a useless webhook application"
-VERSION="!!V0.0.1"
+VERSION="!!V0.0.2"
 
 enabled() {
 echo $WELCOME;
@@ -10,11 +10,14 @@ echo $VERSION;
 
 enabled
 
+while read url; do
+	echo "successfully found webhook url"
+done < url.txt;
+
 text=${1}
-url=${2}
 
 [[ -z ${1} ]] && echo provide text && read text;
-[[ -z ${2} ]] && echo provide a valid webhook URL && read url;
+[[ -z ${url} ]] && echo provide a valid webhook URL in url.txt && exit 1;
 
 send() {
 msg_content=" { \"content\": \"$text\" }";
